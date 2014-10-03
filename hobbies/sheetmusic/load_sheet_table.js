8623ed7b086200606ea9080f21c374496c09f268
Eric Lam <marcusgladir@gmail.com>
2014-10-02 20:44:24 -0400

    Add testing functions

---
+++ hobbies/sheetmusic/load_sheet_table.js
@@ -0,0 +1,26 @@
+// Change url after
+//var url = "http://ericblam.com/hobbies/sheetmusic/sheet_data.csv";
+var url = "sheet_data.csv";
+data_file = httpGet(url);
+document.write(data_file);
+
+function httpGet(url)
+{
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
