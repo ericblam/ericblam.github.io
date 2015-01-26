70af679617b4eb25eeec235566312e788b174618
Eric Lam <marcusgladir@gmail.com>
2015-01-26 10:56:13 -0500

    Change base_test to use better logic

--- hobbies/sheetmusic/load_sheet_table.js
+++ hobbies/sheetmusic/load_sheet_table.js
@@ -1,10 +1,11 @@
 // Change url after
-//var url = "http://ericblam.com/hobbies/sheetmusic/sheet_data.csv";
+//var url = "http://homepages.rpi.edu/~lame3/hobbies/sheetmusic/sheet_data.csv";
+var url = "http://ericblam.com/hobbies/sheetmusic/sheet_data.csv";
 //var url = "sheet_data.csv";
-var url = "https://raw.githubusercontent.com/ericthelam73/ericthelam73.github.io/master/hobbies/sheetmusic/sheet_data.csv";
 
 function httpGet(url)
 {
+    var xmlhttp;
     if (window.XMLHttpRequest)
     {// code for IE7+, Firefox, Chrome, Opera, Safari
         xmlhttp=new XMLHttpRequest();
@@ -17,14 +18,18 @@ function httpGet(url)
     {
         if (xmlhttp.readyState==4 && xmlhttp.status==200)
         {
-            return xmlhttp.responseText;
+            // return xmlhttp.responseText;
+	    // document.write(xmlhttp.responseText);
+	    // document.getElementById("test").innerHTML = "hello";
+	    document.getElementById("test").innerHTML = xmlhttp.responseText;
         }
     }
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
 }
 
-data_file = httpGet(url);
-document.write(data_file);
-//document.getElementById("test").innerHTML = data_file;
+httpGet(url);
+// data_file = httpGet(url);
+// document.write(data_file);
+
 
