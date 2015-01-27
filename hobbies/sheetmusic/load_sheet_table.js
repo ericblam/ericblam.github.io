1c57539830925759820b87dcac739a87455e2a9c
Eric Lam <marcusgladir@gmail.com>
2015-01-26 22:23:39 -0500

    Finish load_sheet_table function

--- hobbies/sheetmusic/load_sheet_table.js
+++ hobbies/sheetmusic/load_sheet_table.js
@@ -1,7 +1,4 @@
-// Change url after
-// var url = "http://homepages.rpi.edu/~lame3/hobbies/sheetmusic/sheet_data.csv";
 var url = "http://ericblam.com/hobbies/sheetmusic/sheet_data.csv";
-//var url = "sheet_data.csv";
 
 var data_file;
 
@@ -29,5 +26,64 @@ function httpGet(url)
     return file_data;
 }
 
+function write_table(data) {
+    var lines = data.split("\n");
+    var not_first_line = false;
+
+    var text = "";
+
+    var types = lines[0].split(";");
+    var header = lines[1].split(";");
+    var images = lines[2].split(";");
+    var file_index = types.indexOf("file");
+
+    for (i = 3; i < lines.length - 1; i++) {
+	line = lines[i].trim().split(";");
+	// Title
+	if (line[0] == "###") {
+	    // End of Table
+	    if (line[1] == "###")
+		break;
+	    else {
+		if (not_first_line) {
+		    text += "</table><br /><br />";
+		}
+		else {
+		    not_first_line = true;
+		}
+		text += "\t<h3 class='sheetmusic'>";
+		text += line[1];
+		text += "</h3>\n";
+		text += "\t<table class='sheetmusic'>\n";
+		text += "\t\t<tr>\n";
+		for (j = 0; j < header.length; j++) {
+		    text += "\t\t\t<th>" + header[j] + "</th>\n";
+		}
+	    }
+	}
+	// Content
+	else {
+	    text += "\t\t<tr>\n";
+	    for (j = 0; j < file_index; j++) {
+		text += "\t\t\t<td>";
+		text += line[j];
+		text += "</td>\n";
+	    }
+	    for (j = file_index + 1; j < types.length; j++) {
+		text += "\t\t\t<td>";
+		if (line[j] == "y") {
+		    text += "<a href='" + line[file_index] + "." + types[j];
+		    text += "'><img src='" + images[j] + "'></a>";
+		}
+		text += "</td>\n";
+	    }
+	    text += "</tr>\n";
+	}
+    }
+    text += "</table>";
+    return text;
+}
+
 data_file = httpGet(url);
-document.getElementById("test").innerHTML = data_file;
+var formatted_table = write_table(data_file);
+document.getElementById("test").innerHTML = formatted_table;
